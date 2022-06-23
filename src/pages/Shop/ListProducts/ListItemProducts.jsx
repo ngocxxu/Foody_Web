0cdbe3e358b2170314b07fbf48b339aff5864ffc
ngocxxu <ngocquach43@gmail.com>
2022-06-23 23:53:34 +0700

    create 'handleArrayDataProductList' method

--- src/pages/Shop/ListProducts/ListItemProducts.jsx
+++ src/pages/Shop/ListProducts/ListItemProducts.jsx
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { memo } from 'react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
 import { LazyLoading } from '../../../components/LazyLoading/LazyLoading';
 import { ProductItem } from '../../../components/ProductItem/ProductItem';
 import { getAllProducts } from '../../../services/ProductsService';
@@ -13,6 +14,7 @@ export const ListItemProducts = memo(() => {
   const { dataProductList } = useSelector((state) => state.productReducer);
   const { isLazyLoading } = useSelector((state) => state.othersReducer);
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
 
   const [dataTable, setDataTable] = useState({
     minValue: 0,
@@ -40,13 +42,48 @@ export const ListItemProducts = memo(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
 
+  // Prepare f.slug === 'burgers' - pathname.split('/').filter((i) => i).pop() === 'burgers'
+  const handleArrayDataProductList = useCallback(() => {
+    const newDataProductList = dataProductList.filter((item) =>
+      item.categories.find(
+        (f) =>
+          f.slug ===
+          pathname
+            .split('/')
+            .filter((i) => i)
+            .pop()
+      )
+    );
+
+    // Handle if URL only contain Shop (ex: /shop) and didn't have any category besides (ex: /shop/sauces)
+    if (pathname.split('/').filter((i) => i).length <= 1) {
+      return dataProductList;
+    } else {
+      return newDataProductList;
+    }
+  }, [dataProductList, pathname]);
+
+  const handleRenderCategoryFood = useCallback(() => {
+    return handleArrayDataProductList()
+      .slice(dataTable.minValue, dataTable.maxValue)
+      .map((product) => {
+        return (
+          <div key={product.id}>
+            <ProductItem product={product} />
+          </div>
+        );
+      });
+  }, [handleArrayDataProductList, dataTable.minValue, dataTable.maxValue]);
+
   return (
     <div className="mb-12">
       <div className="lg:flex justify-between items-center lg:ml-0 ml-4">
         <div className="hidden lg:block">
-          {`Showing ${dataTable.minValue + 1} - ${dataTable.maxValue} of ${
-            dataProductList.length
-          } item(s)`}
+          {`Showing ${dataTable.minValue + 1} - ${
+            handleArrayDataProductList().length < 9
+              ? handleArrayDataProductList().length
+              : dataTable.maxValue
+          } of ${handleArrayDataProductList().length} item(s)`}
         </div>
         <SortProduct />
       </div>
@@ -55,18 +92,17 @@ export const ListItemProducts = memo(() => {
       ) : (
         <>
           <div className="lg:grid lg:grid-cols-3 gap-2 ">
-            {dataProductList !== null &&
-              dataProductList
-                .slice(dataTable.minValue, dataTable.maxValue)
-                .map((product) => <ProductItem key={product.id} product={product} />)}
+            {dataProductList !== null && handleRenderCategoryFood()}
           </div>
-          <Pagination
-            className="lg:flex justify-center items-center"
-            defaultCurrent={1}
-            defaultPageSize={9}
-            onChange={handleChange}
-            total={15}
-          />
+          {handleArrayDataProductList().length >= 9 && (
+            <Pagination
+              className="lg:flex justify-center items-center"
+              defaultCurrent={1}
+              defaultPageSize={9}
+              onChange={handleChange}
+              total={15}
+            />
+          )}
         </>
       )}
     </div>
